c000b601004731d5bd30fc5389caf8490ffe9575
JulienVer69 <julien.versaci@gmail.com>
2018-08-19 23:04:09 +0200

    linearSystSolver file added

---
+++ src/linearSystSolver.f90
@@ -0,0 +1,20 @@
+C-----------------------------------------------
+C This module computes the solution of a sparse 
+C symetric linear system : Ax = b
+C A is a sparse matrix 
+C----------------------------------------------- 
+C This module comes from Quantsolver project 
+C
+C Julien Versaci 
+C-----------------------------------------------
+
+module linearSystemSolver
+
+C.. first we have to store the matrix 
+C.. then  we have to check our data store scheme 
+C.. Finally, we have to solve the system and print
+C.. the solution 
+
+
+end module 
+
