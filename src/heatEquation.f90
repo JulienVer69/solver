b30d6986753bf918dfbde220b219112e9c582ba2
JulienVer69 <julien.versaci@gmail.com>
2018-10-07 16:37:26 +0200

    sequential opt adi method fully implemented

--- src/heatEquation.f90
+++ src/heatEquation.f90
@@ -58,6 +58,8 @@ end subroutine
 
               Nt = n*m  
 
+            !call adi_opt_method()
+
             call adi_method()
 
             call clear_memory()
@@ -94,7 +96,7 @@ REAL*8 :: temp
 
         do i_step = 0,time_step 
 
-         write(File_name_output,'(A,I3.3,A)') 'data/GenericName',i_step,'.ext'
+         write(File_name_output,'(A,I3.3,A)') 'data2/GenericName',i_step,'.ext'
          open(unit=13,file = File_name_output)  
 
          if (i_step == 0 ) then 
@@ -105,17 +107,18 @@ REAL*8 :: temp
 
 
 
-        call adi_init(1) 
+        call adi_init(1)
         !call gaussJordanMethod(matrix,cl_tab,Nt)
         call lapack_solver(matrix,cl_tab,Nt,1) 
         
-        x = cl_tab 
+        x = cl_tab
+        
 
         call adi_init(2) 
        ! call gaussJordanMethod(matrix,cl_tab,Nt)
+
         call lapack_solver(matrix,cl_tab,Nt,1)
 
-        
 
 
         do i=1,n 
@@ -130,7 +133,8 @@ REAL*8 :: temp
  
       call write_mesh(13) 
       
-           write(*,*) "calculation time step", i_step , "finished" 
+           write(*,*) "calculation time step", i_step , "finished"
+            
     endif
 
  enddo 
@@ -248,8 +252,181 @@ subroutine adi_init(step)
 
  
            
+end subroutine
+
+
+subroutine adi_opt_method() 
+INTEGER :: i,j,i_step,mini,maxi 
+CHARACTER (len=80) :: file_name_output  
+REAL, dimension(:), allocatable ::  tab
+INTEGER :: k,p 
+
+
+allocate ( matrix(n,m) )
+allocate ( cl_tab(Nt) )
+allocate ( x(Nt) )
+allocate (tab(m))
+
+x = 293.
+
+cl_north = cl_north +273
+cl_south = cl_south +273
+cl_east  = cl_east + 273
+cl_west  = cl_west + 273
+ 
+write(File_name_output,'(A,I3.3,A)') 'data/GenericName',0,'.ext'
+open(unit=13,file = File_name_output) 
+CALL  write_mesh(13)
+
+        do i_step = 1,time_step
+           write(File_name_output,'(A,I3.3,A)') 'data/GenericName',i_step,'.ext'
+
+           
+           open(unit=13,file = File_name_output)
+           do i=1,n
+                mini = (i-1)*m +1 
+                maxi = i*m 
+                CALL adi_opt_init(1,i,tab)
+                CALL lapack_solver(matrix,tab,n,1)
+                cl_tab(mini:maxi) = tab
+           enddo
+ 
+ x = cl_tab
+ cl_tab=0.
+           do i=1,n
+                mini = (i-1)*m +1 
+                maxi = i*m   
+               CALL adi_opt_init(2,i,tab)
+               CALL lapack_solver(matrix,tab,n,1)
+               cl_tab(mini:maxi) = tab
+           enddo
+
+           
+     do i=1,n 
+          do j=1,m
+           
+              k = (i-1)*m + j 
+              p = (j-1)*n + i
+
+              x(k) = cl_tab(p) 
+         enddo 
+        enddo       
+
+
+
+            CALL write_mesh(13) 
+          enddo  
+
+deallocate(tab)
+
+end subroutine
+
+subroutine adi_opt_init(step,line,tab)
+INTEGER :: i,j,k,p, tab_ind
+INTEGER :: step
+INTEGER :: line
+REAL :: tab(:) 
+!INTEGER :: mini,maxi 
+
+matrix =0. 
+tab =0.
+
+do i = 1,n           
+   do j=1,m
+      ! A matrix
+      if ( i == j ) then 
+         matrix(i,j) = 1.0 +4.0*alpha  
+      end if
+      if (j == i+1 .and. i < n ) then  
+         matrix(i,j) = -2.0*alpha 
+      end if
+      if (  j == i-1 .and. i>1 ) then 
+         matrix(i,j) = -2.0*alpha 
+      end if
+    enddo
+enddo
+
+
+     if ( step == 1) then
+
+do j=1,m
+   i=line
+   
+   k = (i-1)*m +j
+
+
+          tab(j) = (1 - 4.0*alpha)*x(k)
+! top side of the point  
+          if ( i == 1) then       
+            tab(j) = tab(j) + 2.0*alpha*cl_north 
+          else
+            tab(j) = tab(j) + 2.0*alpha*x(k-m)
+          endif 
+
+
+! lower side of the point 
+          if ( i == n ) then 
+            tab(j) =  tab(j) + 2.0*alpha*cl_south  
+          else
+            tab(j) =  tab(j) + 2.0*alpha*x(k+m)
+          endif  
+
+          
+! left side of the point
+          if ( j == 1 ) then 
+            tab(j) =  tab(j) +  2.0*alpha*cl_west
+          endif 
+
+
+! right side of the point            
+          if ( j == m ) then  
+            tab(j) = tab(j)  + 2.0*alpha*cl_east  
+          endif
+
+
+   enddo
+endif 
+
+        if ( step == 2 ) then 
+
+do tab_ind =1,n
+       j = line  
+
+           p = (tab_ind-1)*n + j
+
+           tab(tab_ind) = (1 - 4.0*alpha)*x(p)
+
+! top side of the point 
+          if ( tab_ind == 1) then             
+           tab(tab_ind) =  tab(tab_ind) + 2.0*alpha*cl_north  
+          endif  
+! lower side of the point  
+          if ( tab_ind == n ) then 
+           tab(tab_ind) =  tab(tab_ind)  + 2.0*alpha*cl_south 
+          endif 
+! left side of the point 
+          if ( j == 1 ) then 
+            tab(tab_ind) =  tab(tab_ind) + 2.0*alpha*cl_west
+          else 
+            tab(tab_ind) =  tab(tab_ind) + 2.0*alpha*x(p-1) 
+          endif 
+! right side of the point            
+          if ( j == m ) then  
+            tab(tab_ind) =  tab(tab_ind) + 2.0*alpha*cl_east    
+          else
+            tab(tab_ind) =  tab(tab_ind) + 2.0*alpha*x(p+1)     
+          endif 
+  
+enddo
+     endif 
+
+
+
+
 end subroutine 
 
+
+                                    
 
        subroutine write_mesh(out_unit) 
        INTEGER :: i,j,k
@@ -292,7 +469,8 @@ end subroutine
     subroutine clear_memory() 
      deallocate(x)
      deallocate(cl_tab) 
-     deallocate(matrix) 
+     deallocate(matrix)
+
   !   deallocate( cl_south ) 
   !   deallocate( cl_north ) 
   !   deallocate( cl_east ) 
